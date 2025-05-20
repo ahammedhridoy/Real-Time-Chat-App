34a0ee8a6c74051adfea394c53f45389b1345bb9
ahammedhridoy <developerhridoy@gmail.com>
2025-05-20 22:53:00 +0600

    Almost Done

---
+++ frontend/src/components/SendInput.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { IoSend } from "react-icons/io5";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setMessages } from "../redux/messageSlice";
+import { BASE_URL } from "..";
+
+const SendInput = () => {
+  const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+  const { selectedUser } = useSelector((store) => store.user);
+  const { messages } = useSelector((store) => store.message);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/api/v1/message/send/${selectedUser?._id}`,
+        { message },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      dispatch(setMessages([...messages, res?.data?.newMessage]));
+    } catch (error) {
+      console.log(error);
+    }
+    setMessage("");
+  };
+  return (
+    <form onSubmit={onSubmitHandler} className="px-4 my-3">
+      <div className="relative w-full">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          placeholder="Send a message..."
+          className="border text-sm rounded-lg block w-full p-3 border-[#E3EEB2] bg-[#E3EEB2] text-black"
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 flex items-center pr-4 end-0"
+        >
+          <IoSend />
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SendInput;
