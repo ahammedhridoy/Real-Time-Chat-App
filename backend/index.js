59cadad3a642008d2f27bf55a9d9e3a44b0be548
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2025-05-25 01:34:58 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3000",
+  origin: "https://real-time-chat-app-frontend-gamma.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOption));
