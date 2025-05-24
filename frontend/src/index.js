8026f96225865888a21b2a43a810ab4934173dce
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2025-05-25 01:41:09 +0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { persistStore } from "redux-persist";
 
 let persistor = persistStore(store);
 
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = "https://real-time-chat-app-backend-delta.vercel.app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
